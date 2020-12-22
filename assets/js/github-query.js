7ae95cd268f77dcc29c060c7bfbc3d7388d47f78
tchamalsha <62301204+tchamalsha@users.noreply.github.com>
2020-12-22 16:46:51 +0530

    Fetch data and display in the web site

--- assets/js/github-query.js
+++ assets/js/github-query.js
@@ -2,10 +2,10 @@ let repos=document.getElementById("my-github-projects")
 const gitURL="https://gh-pinned-repos-5l2i19um3.vercel.app/?username=tchamalsha"
 
 fetch(gitURL).then(function(response){
-    return response.json;
+    return response.json();
 }).then(function(object){
     data = object
-
+    
     data.forEach(element => {
         repo = element.repo
         url = element.link
@@ -19,10 +19,28 @@ fetch(gitURL).then(function(response){
     })
 }).catch(function(error){
     alert("Something went wrong!!")
-    console.error(error)
+    console.log(error)
 })
 function pushRepositories(repoName,repoURL,repoDescription,repoLanguage,repoStars,repoForks){
-    let repoTemplate =
+    let repoTemplate =`
+        <div class="row">
+            <div class="col-sm-12 justify-content-center d-flex">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${repoName}</h5>
+                        <p class="card-text">${repoDescription}
+                        </p>
+                        <div class="git-icon">
+                            <a href="${repoURL}">
+                                <i class="fa fa-github"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+    repos.innerHTML += repoTemplate
     
 }
  
